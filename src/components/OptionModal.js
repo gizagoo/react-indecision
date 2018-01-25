f08eefa19522051675fa313a253f3b719358f80b
Frank Murphy <frank.nds@gmail.com>
2018-01-25 12:40:38 +0000

    End Of Part One

--- src/components/OptionModal.js
+++ src/components/OptionModal.js
@@ -6,10 +6,11 @@ const OptionModal = (props) => (
         onRequestClose={props.closeSelectedOptionModal} 
         contentLabel="Selected Option"
         closeTimeoutMS={200}
+        className="modal"
         >
-        <h3>Selected Option</h3>
-        <p>{props.selectedOption}</p>
-        <button onClick={props.closeSelectedOptionModal}>I'm on it!</button>
+        <h3 className = "modal__title">Selected Option</h3>
+        <p className="modal__body">{props.selectedOption}</p>
+        <button className="button" onClick={props.closeSelectedOptionModal}>I'm on it!</button>
     </Modal>
 );
 
